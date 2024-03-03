c26fb0a40e5e4b5acd14f5601aa0c85ff98446ae
aksel <pawelaksel@gmail.com>
2024-03-03 16:06:41 +0100

    add api project

---
+++ GymHero.API/Controllers/WorkoutController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GymHero.API.Controllers;
+
+[ApiController]
+[Route("workout")]
+public class WorkoutController : ControllerBase
+{
+    [HttpGet("/ping")]
+    public string Ping()
+    {
+        return DateTimeOffset.Now.ToString();
+    }
+    
+}
